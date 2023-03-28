918800a12b8838711e92e49815331c58c6b5075c
Vladislav <vladislav.v.o@yandex.ru>
2023-03-28 17:17:31 +0300

    done

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+    	tree.insert(5);
+    	tree.insert(3);
+    	tree.insert(7);
+    	tree.insert(1);
+    	tree.insert(9);
+    	tree.insert(6);
+	}
+}
